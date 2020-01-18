85f7b83b333d13446b40b934fde5f1c9472f052b
Ussama Azam <nuclearoreo@gmail.com>
2020-01-18 14:13:02 -0500

    Bot basic streaming set

--- src/bot.ts
+++ src/bot.ts
@@ -2,9 +2,23 @@ import Twitter from 'twitter'
 
 require('dotenv').config()
 
+// Setting Up the Client
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY || '',
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET || '',
   access_token_key: process.env.TWITTER_TOKEN_KEY || '',
   access_token_secret: process.env.TWITTER_TOKEN_SECRET || ''
+})
+
+// Created a stream Object to stream from twitter
+const stream = client.stream('statuses/filter', { track: 'javascript' })
+
+// Printing tweets
+stream.on('data', event => {
+  console.log(event && event.text)
+})
+
+//If anything goes wrongs
+stream.on('error', error => {
+  throw error
 })
