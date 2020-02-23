3096b9dbf75b8ed723a10b476051a7f9f99b6421
Ussama Azam <nuclearoreo@gmail.com>
2020-02-23 12:54:49 -0500

    Organized the bot into a class

--- src/bot.ts
+++ src/bot.ts
@@ -4,28 +4,38 @@ import Cities from './types/cities'
 
 require('dotenv').config()
 
-// Grabbing all the bounding boxes for each city
-const cities: Cities = config.get('cities')
+export default class Bot {
+  client: Twitter
+  cities: Cities
+  stream: NodeJS.EventEmitter
 
-// Setting Up the Client
-const client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY || '',
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || '',
-  access_token_key: process.env.TWITTER_TOKEN_KEY || '',
-  access_token_secret: process.env.TWITTER_TOKEN_SECRET || ''
-})
+  constructor() {
+    // Grabbing all the bounding boxes for each city
+    this.cities = config.get('cities')
 
-// Created a stream Object to stream from twitter
-const stream = client.stream('statuses/filter', {
-  locations: cities.chicago
-})
+    // Setting Up the Client
+    this.client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY || '',
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET || '',
+      access_token_key: process.env.TWITTER_TOKEN_KEY || '',
+      access_token_secret: process.env.TWITTER_TOKEN_SECRET || ''
+    })
 
-// Printing tweets
-stream.on('data', event => {
-  console.log(event && event.text)
-})
+    // Created a stream Object to stream from twitter
+    this.stream = this.client.stream('statuses/filter', {
+      locations: this.cities.chicago
+    })
+  }
 
-//If anything goes wrongs
-stream.on('error', error => {
-  throw error
-})
+  run(): void {
+    // Printing tweets
+    this.stream.on('data', event => {
+      console.log(event && event.text)
+    })
+
+    //If anything goes wrongs
+    this.stream.on('error', error => {
+      throw error
+    })
+  }
+}
