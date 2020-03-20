6d62dfddf3815a67e8139a76a0ab2e50a41bd34c
Ussama Azam <nuclearoreo@gmail.com>
2020-03-19 21:45:58 -0400

    Changing approach not using API, going to use database

--- src/utilities/quotes.ts
+++ src/utilities/quotes.ts
@@ -1,9 +1,3 @@
-import axios, { AxiosInstance } from 'axios'
-
 export default class Quotes {
-  client: AxiosInstance
-
-  constructor() {
-    this.client = axios.create()
-  }
+  constructor() {}
 }
