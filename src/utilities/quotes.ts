6a8e852e26f4946aba9fc13ec08c1a3c374b4899
Ussama Azam <nuclearoreo@gmail.com>
2020-08-09 22:44:35 -0400

    Attempting to to grab qoutes

--- src/utilities/quotes.ts
+++ src/utilities/quotes.ts
@@ -1,3 +1,17 @@
+import { MongoClient } from 'mongodb'
+
 export default class Quotes {
-  constructor() {}
+  private client: any
+
+  constructor() {
+    this.setup()
+  }
+
+  private async setup(uri = 'mongodb://localhost:27017') {
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+    }).catch((err) => console.log(err))
+  }
+
+  async getQuotes() {}
 }
